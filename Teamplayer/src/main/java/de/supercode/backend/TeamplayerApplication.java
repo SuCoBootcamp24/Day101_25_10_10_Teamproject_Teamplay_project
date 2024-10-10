b35f8086ef5251886707d622e348fd0042f15da5
René Behrens <rene.behrens@aol.com>
2024-10-10 11:01:54 +0200

    created new project files

---
+++ Teamplayer/src/main/java/de/supercode/backend/TeamplayerApplication.java
@@ -0,0 +1,13 @@
+package de.supercode.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamplayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamplayerApplication.class, args);
+    }
+
+}
