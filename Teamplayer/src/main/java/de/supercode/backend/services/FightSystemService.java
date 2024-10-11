92fe1ccdaf79a15a6867d4c35e6435e966b34d3c
René Behrens <rene.behrens@aol.com>
2024-10-11 18:35:13 +0200

    create experimentel Fightsystem

--- Teamplayer/src/main/java/de/supercode/backend/services/FightSystemService.java
+++ Teamplayer/src/main/java/de/supercode/backend/services/FightSystemService.java
@@ -22,19 +22,19 @@ public class FightSystemService {
     }
 
     public boolean standardFightSystem(User initUser, User enemy) {
-
         List<Player> teamUser = new ArrayList<>(initUser.getTeam().getPlayers());
         List<Player> teamEnemy = new ArrayList<>(enemy.getTeam().getPlayers());
 
         boolean isDraw = true;
 
-
-        while (!isDraw) {
+        while (isDraw) {
             int userWins = 0;
             int enemyWins = 0;
 
             Collections.shuffle(teamUser);
+            System.out.println(teamUser);
             Collections.shuffle(teamEnemy);
+            System.out.println(teamEnemy);
 
             for (int i = 0; i < teamUser.size(); i++) {
                 Player playerUser = teamUser.get(i);
@@ -67,6 +67,51 @@ public class FightSystemService {
             }
         }
         return false; // if draw
+    }
+
+    public void experimentalFightSystem(User initUser, User enemy){
+        List<Player> teamUser = new ArrayList<>(initUser.getTeam().getPlayers());
+        List<Player> teamEnemy = new ArrayList<>(enemy.getTeam().getPlayers());
+
+        boolean isDraw = true;
+
+        while (isDraw) {
+
+            Collections.shuffle(teamUser);
+            System.out.println(teamUser);
+            Collections.shuffle(teamEnemy);
+            System.out.println(teamEnemy);
+
+            for (int teamUserPlayers = 0; teamUserPlayers < teamUser.size(); teamUserPlayers++) {
+                Player userPlayer = teamUser.get(teamUserPlayers);
+                if (userPlayer.getPowerlevel() > 0 ) {
+                    for (int teamEnemyPlayers = 0; teamEnemyPlayers < teamEnemy.size(); teamEnemyPlayers++) {
+                        Player enemyPlayer = teamEnemy.get(teamEnemyPlayers);
+                        System.out.println();
+                        System.out.println("before = U: " + userPlayer.getPowerlevel() + " E: " + enemyPlayer.getPowerlevel());
+                        if (enemyPlayer.getPowerlevel() > 0 ) {
+
+                            if (userPlayer.getPowerlevel() > enemyPlayer.getPowerlevel()) {
+                                userPlayer.setPowerlevel(userPlayer.getPowerlevel() - enemyPlayer.getPowerlevel());
+                                enemyPlayer.setPowerlevel(0);
+                                System.out.println("after U = U: " + userPlayer.getPowerlevel() + " E: " + enemyPlayer.getPowerlevel());
+
+                            } else if (userPlayer.getPowerlevel() < enemyPlayer.getPowerlevel()) {
+                                enemyPlayer.setPowerlevel(enemyPlayer.getPowerlevel() - userPlayer.getPowerlevel());
+                                userPlayer.setPowerlevel(0);
+                                System.out.println("after E = U: " + userPlayer.getPowerlevel() + " E: " + enemyPlayer.getPowerlevel());
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            }
+            break;
+
+        }
+        //return false; // if draw
+
     }
 
 }
